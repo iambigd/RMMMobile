f14730fb1068a2ddd7275466ff03db3c71ec287f
Ken <bigdstut@gmail.com>
2016-02-26 13:57:27 +0800

    update delegate

--- RMMMobile/AppDelegate.swift
+++ RMMMobile/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  RMMMobile
+//  RMM
 //
-//  Created by wei chih tsai on 2016/2/26.
+//  Created by wei chih tsai on 2016/1/14.
 //  Copyright © 2016年 ECG. All rights reserved.
 //
 
@@ -10,37 +10,70 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    //user login or not
+    //    var userLoggedIn = true
+    var userLoggedIn = false
+    
+    let log =  LogSwift.logger
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        log.setup(.Debug, showThreadName: true, showLogLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: "path/to/file", fileLogLevel: .Debug)
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        if( userLoggedIn) {
+            
+            log.info("user have login")
+            
+            let tabBarController: UITabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+            
+            let navigationController = self.window?.rootViewController as! UINavigationController
+            //            navigationController.navigationBarHidden = true
+            navigationController.setViewControllers([tabBarController], animated: true)
+            
+        } else {
+            log.warning("uesr not authorization (go to LoginVC)")
+            
+            let loginVC = storyboard.instantiateViewControllerWithIdentifier("LoginVC") as! LoginVC
+            
+            if self.window!.rootViewController as? UINavigationController != nil {
+                let navigationController = self.window?.rootViewController as! UINavigationController
+                //                navigationController.navigationBarHidden = true
+                navigationController.setViewControllers([loginVC], animated: true)
+                
+            }
+        }
+        
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
