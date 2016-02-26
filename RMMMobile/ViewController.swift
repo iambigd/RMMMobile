001a435a46d77ef6a13c1f2fc8f3e45c875c0e0b
Ken <bigdstut@gmail.com>
2016-02-26 11:00:03 +0800

    Initial Commit

---
+++ RMMMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RMMMobile
+//
+//  Created by wei chih tsai on 2016/2/26.
+//  Copyright © 2016年 ECG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
